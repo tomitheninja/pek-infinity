28163a0b99aa9f6942af367d896f8fcfd35f5f32
tomitheninja <sudi.tamas@simonyi.bme.hu>
2024-07-22 01:48:14 +0200

    Use absolute paths in next configs

---
+++ frontend/postcss.config.js
@@ -0,0 +1,9 @@
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: __dirname + "/tailwind.config.js",
+    },
+    autoprefixer: {},
+  },
+};
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    __dirname + "/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    __dirname + "/components/**/*.{js,ts,jsx,tsx,mdx}",
+    __dirname + "/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
