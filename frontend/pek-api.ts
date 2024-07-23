42903617252f378dce4576f3c5d64b2d10acc893
Tamás Südi <sudi.tamas@simonyi.bme.hu>
2024-07-23 12:37:23 +0200

    Vercel environment (#9)
    * Configure API URL
    
    * Add vercel.json
    
    Co-authored-by: Gergő Bujdosó <bujgergo@gmail.com>
    
    ---------
    
    Co-authored-by: Gergő Bujdosó <bujgergo@gmail.com>

--- frontend/pek-api.ts
+++ frontend/pek-api.ts
@@ -1,12 +1,22 @@
 import { DefaultApi, Configuration } from '../pek-client';
 
+function getBasePath(): string {
+  if (process.env.NEXT_PUBLIC_API_URL) {
+    return process.env.NEXT_PUBLIC_API_URL;
+  }
+  if (process.env.VERCEL_ENV === 'preview') {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- if VERCEL_ENV is preview, VERCEL_URL is set
+    return `https://${process.env.VERCEL_URL!}`;
+  }
+  return 'http://localhost:3000';
+}
+
+// // eslint-disable-next-line no-console -- .
+// console.log('API URL:', getBasePath());
+
 export class PekApi {
   private get api(): DefaultApi {
-    return new DefaultApi(
-      new Configuration({
-        basePath: process.env.NEXT_PUBLIC_API ?? 'http://localhost:3000',
-      }),
-    );
+    return new DefaultApi(new Configuration({ basePath: getBasePath() }));
   }
 
   async ping(): Promise<string> {
