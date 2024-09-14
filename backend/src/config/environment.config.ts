d23592e6a540cc68d1acc681a3b82137e58b8f8f
Tamás Südi <sudi.tamas@simonyi.bme.hu>
2024-09-14 02:42:01 +0200

    Fix vercel preview environment (#31)

--- backend/src/config/environment.config.ts
+++ backend/src/config/environment.config.ts
@@ -3,9 +3,12 @@ import * as env from 'env-var';
 
 dotenv.config();
 
+const fallbackToPreviewFrontend =
+  process.env.VERCEL_ENV === 'preview' || process.env.FORCE_LAMBDA_API === '1';
+
 export const FRONTEND_CALLBACK =
-  process.env.VERCEL_ENV === 'preview' || process.env.FORCE_LAMBDA_API === '1'
-    ? `https://${process.env.VERCEL_BRANCH_URL!}`
+  !process.env.FRONTEND_CALLBACK && fallbackToPreviewFrontend
+    ? process.env.VERCEL_BRANCH_URL!
     : env.get('FRONTEND_CALLBACK').required().asString();
 
 export const JWT_SECRET = env.get('JWT_SECRET').required().asString();
